0cbc117cd33d36d3f70fb221691c9da26c93632e
A-Katsuta <220499870+A-Katsuta@users.noreply.github.com>
2026-03-30 00:23:04 +0900

    Sync current workspace state

--- test/tools/user/run_self_test.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-for %%I in ("%~dp0..\..") do set "ROOT_DIR=%%~fI\"
-set "PS_EXE=powershell.exe"
-where pwsh >nul 2>nul && set "PS_EXE=pwsh"
-"%PS_EXE%" -NoLogo -NoProfile -ExecutionPolicy Bypass -File "%ROOT_DIR%scripts\run_self_test.ps1" %*
